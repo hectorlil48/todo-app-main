50d1ca04a9a50a9936bc55bf0cf0fb48f35734ee
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2025-09-03 17:37:44 -0500

    Changed some colors, added text color for light and dark mode on the input. Removed the focus styles.

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -3,22 +3,26 @@ import { useState } from "react";
 function TodoInput({ onAddTodo }) {
   const [inputText, setInputText] = useState("");
 
-  const handleChange = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     if (inputText.trim() === "") return;
 
-    onAddTodo(inputValue.trim());
+    onAddTodo(inputText.trim());
     setInputText("");
   };
   return (
-    <form onSubmit={handleChange} className="w-full mb-6">
+    <form onSubmit={handleSubmit} className="w-full mb-6">
       <div className="relative">
+        <label htmlFor="todo-input" className="sr-only">
+          Add a new todo
+        </label>
         <input
           type="text"
           value={inputText}
+          id="todo-input"
           onChange={(e) => setInputText(e.target.value)}
           placeholder="Create a new todo..."
-          className="w-full bg-white font-display dark:bg-input rounded-[5px] py-[14px] pl-[52px] pr-4 placeholder:text-xs placeholder:font-normal placeholder:tracking-[-0.17px] placeholder-gray-400 dark:placeholder-gray-600 transition-all duration-500"
+          className="w-full bg-white font-display dark:bg-input rounded-[5px] py-[14px] pl-[52px] pr-4 placeholder:text-xs text-xs sm:text-lg font-normal tracking-[-0.17px] sm:tracking-[-0.25px] placeholder:font-normal placeholder:tracking-[-0.17px] placeholder-gray-400 text-inputText dark:text-inputText dark:placeholder-gray-600 transition-all duration-500 focus:outline-none"
         />
         {/* Circle checkbox for visual design */}
         <div className="absolute left-5 top-1/2 transform -translate-y-1/2">
